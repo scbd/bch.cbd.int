d053931a8cb186015f9cedc871341d26ae1d1dc4
Stephane Bilodeau <bilodeaustephane@gmail.com>
2014-07-23 10:23:51 -0400

    Update package versions Add Timestamp to console Log Unhandled exceptions

--- server.js
+++ server.js
@@ -1,27 +1,34 @@
 'use strict';
 /* jshint node:true */
-var fs = require('fs');
-var http = require('http');
-var express = require('express');
 
-// Create server
+require('console-stamp')(console, 'HH:MM:ss.l');
 
-var app = express();
-var server = http.createServer(app);
-var oneDay = 24*60*60*1000;
-
-app.configure(function() {
-    app.set('port', process.env.PORT || 2050, '127.0.0.1');
-    app.use(express.logger('dev'));
-    app.use(express.compress());
-    app.use('/app', express.static(__dirname + '/app'));
+// LOG UNHANDLED EXCEPTION AND EXIT
+process.on('uncaughtException', function (err) {
+  console.error('uncaughtException:', err.message);
+  console.error(err.stack);
+  process.exit(1);
 });
+
+/////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
+var express = require('express');
+var app     = express();
+var server  = require('http').createServer(app);
+
+app.set('port', process.env.PORT || 2050);
+app.use(express.logger('dev'));
+app.use(express.compress());
 
 // Configure routes
 
-app.get('*', function(req, res) { res.send('404', 404); } );
+app.use('/app', express.static(__dirname + '/app'));
+app.all('*', function(req, res) { res.send('404', 404); } );
 
 // Start server
 
-console.log('Server listening on port ' + app.get('port'));
-server.listen(app.get('port'));
+server.listen(app.get('port'), '0.0.0.0');
+server.on('listening', function () {
+    console.info('info: BCH started. Listening on %j', this.address());
+});
