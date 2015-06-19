7f04be38cb03349a61b67c2575944ce83f6caf5f
Stephane Bilodeau <bilodeaustephane@gmail.com>
2015-06-19 14:46:26 -0400

    Cleanup server.js

--- server.js
+++ server.js
@@ -1,18 +1,3 @@
-/* jshint node: true, browser: false */
-'use strict';
-
-require("console-stamp")(console, "HH:MM:ss.l");
-
-// LOG UNHANDLED EXCEPTION AND EXIT
-process.on('uncaughtException', function (err) {
-  console.error('uncaughtException:', err.message);
-  console.error(err.stack);
-});
-
-/////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////
-
 // CREATE HTTP SERVER AND PROXY
 
 var app     = require('express')();
@@ -23,16 +8,12 @@ var proxy   = require('http-proxy').createProxyServer({});
 app.use(require('morgan')('dev'));
 app.use(require('compression')());
 
-app.use('/app',         require('serve-static')(__dirname + '/app_build'));
-app.use('/app',         require('serve-static')(__dirname + '/app'));
-app.all('/app/*',       function(req, res) { res.status(404).send(); } );
-
 // Configure routes
 
-app.get   ('/api/*', function(req, res) { proxy.web(req, res, { target: 'https://api.cbd.int:443', secure: false } ); } );
-app.put   ('/api/*', function(req, res) { proxy.web(req, res, { target: 'https://api.cbd.int:443', secure: false } ); } );
-app.post  ('/api/*', function(req, res) { proxy.web(req, res, { target: 'https://api.cbd.int:443', secure: false } ); } );
-app.delete('/api/*', function(req, res) { proxy.web(req, res, { target: 'https://api.cbd.int:443', secure: false } ); } );
+app.use('/app',   require('serve-static')(__dirname + '/app_build'));
+app.use('/app',   require('serve-static')(__dirname + '/app'));
+app.all('/app/*', function(req, res) { res.status(404).send(); } );
+app.all('/api/*', function(req, res) { proxy.web(req, res, { target: 'https://api.cbd.int:443', secure: false } ); } );
 
 // FOR DEV:
 // app.all('/*', function(req, res) { proxy.web(req, res, { target: 'https://bch.cbd.int:443', secure: false } ); } );
@@ -41,12 +22,4 @@ app.delete('/api/*', function(req, res) { proxy.web(req, res, { target: 'https:/
 
 app.listen(process.env.PORT || 2050, function () {
 	console.log('Server listening on %j', this.address());
-});
-
-// LOG PROXY ERROR & RETURN http:500
-
-proxy.on('error', function (e, req, res) {
-    console.error('error proxying: '+req.url);
-    console.error('proxy error:', e);
-    res.send( { code: 500, source:'chm/proxy', message : 'proxy error', proxyError: e }, 500);
 });
