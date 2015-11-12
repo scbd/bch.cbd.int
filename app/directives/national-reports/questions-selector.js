91f6731f88d2bd598d4d9cfa0fe3f87c2d4cac42
Stephane Bilodeau <bilodeaustephane@gmail.com>
2015-11-12 14:45:31 -0500

    Add countries & regions selectors to question selector

--- app/directives/national-reports/questions-selector.js
+++ app/directives/national-reports/questions-selector.js
@@ -1,63 +1,193 @@
-define(['text!./questions-selector.html', 'app', 'lodash', "require"], function(templateHtml, app, _, require) {
+define(['text!./questions-selector.html', 'app', 'lodash', 'require', 'directives/selectors/terms-dialog', 'directives/intermediate'], function(templateHtml, app, _, require) {
 
     var baseUrl = require.toUrl('');
 
-	//==============================================
-	//
-	//
-	//==============================================
-	app.directive('nationalReportQuestionsSelector', ["$http", function($http) {
-		return {
-			restrict : "E",
-			replace : true,
-			template : templateHtml,
-			scope :  {
-			},
-			link: function ($scope) {
+    //==============================================
+    //
+    //
+    //==============================================
+    app.directive('nationalReportQuestionsSelector', ['$http', function($http) {
+        return {
+            restrict : 'E',
+            replace : true,
+            template : templateHtml,
+            scope :  {
+                questions : '=',
+                governments : '='
+            },
+            link: function ($scope) {
 
-                $scope.emitSelection      = emitSelection;
-                $scope.broadcastSelection = broadcastSelection;
-                $scope.reportType         = "cpbNationalReport3";
-
-                $scope.$watch("reportType", loadQuestions);
+                $scope.reportType = 'cpbNationalReport3';
+                $scope.countriesPreset = "cbdRegions";
+                $scope.allSelected = true;
+                $scope.countriesMap = {};
 
                 //====================================
                 //
                 //
                 //====================================
-                function loadQuestions(reportType) {
+                $scope.$watch('reportType', function (reportType) {
 
-                    $http.get(baseUrl+"resources/national-reports/en/"+reportType+".json").then(function(res){
+                    $http.get(baseUrl+'resources/national-reports/en/'+reportType+'.json', { cache : true }).then(function(res){
+
                         $scope.sections = res.data;
+                        $scope.allSelected = true;
+                        $scope.onAllSections();
+
                     });
+                });
+
+                //====================================
+                //
+                //
+                //====================================
+                $scope.$watch('countriesPreset', function (preset) {
+
+                    if(preset=="cbdRegions") { $scope.governments = ["D50FE62D-8A5E-4407-83F8-AFCAAF708EA4","942E40CA-4C23-4D3A-A0B4-736CD0EFCD54","0EC2E5AE-25F3-4D3A-B71F-8019BB62ED4B","19F80933-5D34-46DC-A14C-2867E29ED7CB","93CC7519-2CE8-49CB-8605-DE093B2F6090"]; }
+                    if(preset=="countries")  { $scope.governments = []; $scope.showCountries = true; }
+                    if(preset=="regions")    { $scope.governments = []; $scope.showRegions = true; }
+                });
+
+                //====================================
+                //
+                //
+                //====================================
+                $scope.removeGovernment = function(country){
+
+                    var index = $scope.governments.indexOf(country);
+
+                    if(index>=0)
+                        $scope.governments.splice(index,1);
+                };
+
+                //====================================
+                //
+                //
+                //====================================
+                $scope.onQuestion = applyIntermediateState;
+
+                //====================================
+                //
+                //
+                //====================================
+                $scope.onSection = function(section, applyIntermediate) {
+
+                    section.questions.forEach(function(question){
+                        question.selected = section.selected;
+                    });
+
+                    if(applyIntermediate || applyIntermediate===undefined)
+                    applyIntermediateState();
+                };
+
+                //====================================
+                //
+                //
+                //====================================
+                $scope.onAllSections = function() {
+
+                    if(!$scope.sections)
+                    return;
+
+                    $scope.sections.forEach(function(section){
+                        section.selected = $scope.allSelected;
+                        $scope.onSection(section, false);
+                    });
+
+                    applyIntermediateState();
+                };
+
+                //====================================
+                //
+                //
+                //====================================
+                function applyIntermediateState() {
+
+                    if(!$scope.sections)
+                    return;
+
+                    var on  = 0;
+                    var off = 0;
+                    var int = 0;
+
+                    $scope.sections.forEach(function(section){
+                        section.selected = getIntermediateState(section.questions);
+
+                        if(section.selected===null ) ++int;
+                        if(section.selected===true ) ++on;
+                        if(section.selected===false) ++off;
+                    });
+
+                    if((on && off) || int) $scope.allSelected = null;
+                    else                   $scope.allSelected = !!on;
+
+                    $scope.questions = [];
+
+                    $scope.sections.forEach(function(section){
+                        section.questions.forEach(function(question){
+                            if(question.selected)
+                            $scope.questions.push(question.key);
+                        });
+                    });
+
                 }
 
                 //====================================
                 //
                 //
                 //====================================
-                function emitSelection(question) {
+                function getIntermediateState(items) {
 
-                    var section = _.findWhere($scope.sections, { section : question.section });
+                    var on  = 0;
+                    var off = 0;
 
-                    section.selected = true;
-
-                    section.questions.forEach(function(q){
-                        section.selected = section.selected && q.selected;
+                    items.forEach(function(e) {
+                        if(e.selected) ++on;
+                        else           ++off;
                     });
+
+                    return (on && off) ? null : !!on;
                 }
 
                 //====================================
                 //
                 //
                 //====================================
-                function broadcastSelection(section) {
+                $scope.getCountries = function() {
 
-                    section.questions.forEach(function(q){
-                        q.selected = section.selected
+                    return $http.get("/api/v2013/thesaurus/domains/countries/terms", { cache : true }).then(function (res) {
+                        mapTerms(res.data);
+                        return res.data;
                     });
+                };
+
+                //====================================
+                //
+                //
+                //====================================
+                $scope.getRegions = function() {
+
+                    return $http.get("/api/v2013/thesaurus/domains/regions/terms", { cache : true }).then(function (res) {
+                        mapTerms(res.data);
+                        return res.data;
+                    });
+                };
+
+                $scope.getRegions();
+
+                //====================================
+                //
+                //
+                //====================================
+                function mapTerms(terms) {
+
+                    if(!$scope.countriesMap[terms[0].identifier]) {
+                        terms.forEach(function(t){
+                            $scope.countriesMap[t.identifier] = t;
+                        });
+                    }
                 }
-			}
-		};
-	}]);
+            }
+        };
+    }]);
 });
