248630346bddfb9368cee9abfdbdfdff4713c117
Stephane Bilodeau <bilodeaustephane@gmail.com>
2015-11-18 15:51:26 -0500

    Add color and countries expand

--- app/directives/national-reports/analyzer-question.js
+++ app/directives/national-reports/analyzer-question.js
@@ -1,26 +1,43 @@
-define(['text!./analyzer-question.html', 'app', 'lodash'], function(templateHtml, app, _, require) {
+define(['text!./analyzer-question.html', 'app', 'lodash', 'ngSanitize'], function(templateHtml, app, _) {
+
+    var GREEN = { R:167, G:201, B: 27 };
+    var BLUE  = { R: 70, G:163, B:230 };
+    var WHITE = { R:255, G:255, B:255 };
 
     //==============================================
     //
     //
     //==============================================
-    app.directive('nationalReportAnaylzerQuestion', [function() {
+    app.directive('nationalReportAnalyzerQuestion', [function() {
         return {
             restrict : 'E',
             replace : true,
             template : templateHtml,
+            require : '^nationalReportAnalyzer',
             scope :  {
                 question : '=',
                 reports : '=',
                 regions : '=',
                 sumType : '='
             },
-            link: function ($scope) {
+            link: function ($scope, el, attr, nrAnalyzer) {
 
                 $scope.$watch('::question', analyze);
                 $scope.$watch('::reports',  analyze);
                 $scope.$watch('::regions',  analyze);
 
+                //==============================================
+                //
+                //
+                //==============================================
+                $scope.selectSumType = function() {
+                    nrAnalyzer.selectSumType();
+                };
+
+                //==============================================
+                //
+                //
+                //==============================================
                 function analyze() {
 
                     if(!$scope.reports) return;
@@ -30,16 +47,23 @@ define(['text!./analyzer-question.html', 'app', 'lodash'], function(templateHtml
                     var reports  = $scope.reports;
                     var regions  = $scope.regions;
                     var question = $scope.question;
-                    var optionsMap = _.reduce(question.values, function(map, opt) {
+                    var options  = $scope.question.values;
+                    var optionsMap = {};
 
-                        opt.sum = 0;
-                        opt.percent = 0;
-                        opt.regions = {};
+                    if(!options)
+                        return;
 
-                        map[opt.value] = opt;
+                    options.forEach(function(option, i) {
 
-                        return map;
-                    }, {});
+                        option.sum = 0;
+                        option.percent = 0;
+                        option.regions = {};
+                        option.color = question.type=='option' ? blendColor(GREEN, BLUE, i/(options.length-1)) : BLUE;
+
+                        option.htmlColor = htmlColor(option.color);
+
+                        optionsMap[option.value] = option;
+                    });
 
                     question.sum = 0;
                     question.regions = {};
@@ -56,40 +80,59 @@ define(['text!./analyzer-question.html', 'app', 'lodash'], function(templateHtml
                                 return;
                             }
 
-                            question.sum++; // global
-                            option  .sum++; // row
+                            question.sum++;
+                            option  .sum++;
                             option  .percent = Math.round((option.sum*100) / question.sum);
 
                             regions.forEach(function(region){
 
-                                if(!region.reports[report.government])
-                                    return;
 
                                 var qRegion = question.regions[region.identifier] || (question.regions[region.identifier] = { sum : 0 });
                                 var oRegion = option  .regions[region.identifier] || (option  .regions[region.identifier] = { sum : 0, percentRow: 0, percentColumn: 0, percentGlobal: 0 });
 
-                                qRegion.sum++; // column
-                                oRegion.sum++; // column
+                                if(region.reports[report.government]) {
+                                    qRegion.sum++; // column
+                                    oRegion.sum++; // column
+                                }
 
                                 oRegion.percentRow    = Math.round((oRegion.sum*100) / option.sum);
                                 oRegion.percentColumn = Math.round((oRegion.sum*100) / qRegion.sum);
                                 oRegion.percentGlobal = Math.round((oRegion.sum*100) / question.sum);
+                                oRegion.htmlColor = htmlColor(blendColor(WHITE, option.color, oRegion.percentRow/100));
                             });
                         });
                     });
                 }
 
-                var sumTypes = [
-                    'percentColumn',
-                    'percentRow',
-                    'percentGlobal',
-                    'sum'
-                ];
+                //============================================================
+                //
+                //
+                //============================================================
+                function blendColor(colorA, colorB, ratio) {
+                    return {
+                        R : colorA.R + Math.floor((colorB.R-colorA.R) * ratio),
+                        G : colorA.G + Math.floor((colorB.G-colorA.G) * ratio),
+                        B : colorA.B + Math.floor((colorB.B-colorA.B) * ratio)
+                    };
+                }
 
-                $scope.setSumType = function() {
-                    $scope.sumType = sumTypes.shift();
-                    sumTypes.push($scope.sumType);
-                };
+                //============================================================
+                //
+                //
+                //============================================================
+                function htmlColor(color) {
+
+                    var RR = color.R.toString(16);
+                    var GG = color.G.toString(16);
+                    var BB = color.B.toString(16);
+
+                    if(RR.length<2) RR = '0'+RR;
+                    if(GG.length<2) GG = '0'+GG;
+                    if(BB.length<2) BB = '0'+BB;
+
+                    return '#'+RR+GG+BB;
+                }
+
             }
         };
     }]);
