f371ced39d06d0a4b3b61680f9eaefb0b374504f
Stephane Bilodeau <bilodeaustephane@gmail.com>
2015-11-26 15:50:49 -0500

    Split analyzer & initial question selector

--- app/views/reports/analyzer.js
+++ app/views/reports/analyzer.js
@@ -3,5 +3,32 @@ define(['directives/national-reports/questions-selector', 'directives/national-r
 	return ['$scope', function($scope) {
 
         $scope.showSettings = true;
+
+        try {
+
+            var data = JSON.parse(sessionStorage.getItem('nrAnalyzerData') || '{}');
+
+            $scope.selectedReportType = data.reportType;
+            $scope.selectedQuestions  = data.questions;
+            $scope.selectedRegions    = data.regions;
+
+            if($scope.selectedReportType && $scope.selectedQuestions && $scope.selectedRegions)
+                $scope.showSettings = false;
+
+        } finally {
+            sessionStorage.removeItem('nrAnalyzerData');
+        }
+
+        //========================================
+        //
+        //
+        //========================================
+        $scope.analyze = function(showAnalyser) {
+
+            if(showAnalyser===undefined)
+                showAnalyser = true;
+
+            $scope.showSettings = !showAnalyser;
+        };
     }];
 });
--- app/views/reports/index.js
+++ app/views/reports/index.js
@@ -1,5 +1,20 @@
 define(['directives/national-reports/questions-selector'], function() { 'use strict';
 
-	return [function() {
+	return ['$scope', '$location', function($scope, $location) {
+
+
+        $scope.analyze = function() {
+
+            var data = {
+                reportType : $scope.selectedReportType,
+                questions : $scope.selectedQuestions,
+                regions : $scope.selectedRegions
+            };
+
+            sessionStorage.setItem('nrAnalyzerData', JSON.stringify(data));
+
+            $location.url('/analyzer');
+        };
+
 	}];
 });
