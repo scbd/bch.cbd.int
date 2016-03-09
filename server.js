d21d8106f2b6febee0317c813710d4b0d61b3d8e
Stephane Bilodeau <bilodeaustephane@gmail.com>
2016-03-09 16:14:45 -0500

    Update package version

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 'use strict'; // jshint browser: false, node: true, esnext: true
 // CREATE HTTP SERVER AND PROXY
-var app     = require('express')();
 var proxy   = require('http-proxy').createProxyServer({});
+var express = require('express');
+var app     = new express();
 
 app.use(require('morgan')('dev'));
 
 // Configure routes
 
-app.use('/app',   require('serve-static')(__dirname + '/app'));
+app.use('/app',   express.static(__dirname + '/app'));
 app.all('/app/*', (req, res) => res.status(404).send());
 app.all('/api/*', (req, res) => proxy.web(req, res, { target: 'https://api.cbd.int:443', secure: false } ));
 
